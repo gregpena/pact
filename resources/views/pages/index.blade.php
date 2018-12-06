32fbd59ef78ced11bb64c16fe54adedfc5b75b20
gregpena <gregpena@hotmail.com>
2018-12-06 12:28:21 -0800

    changed main page text

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="jumbotron text-center">
 	<h1>{{$title}}</h1>
-	<p>This is the laravel application from the "Laravel from scratch youtube series"</p>
+	<p>This application will be described soon</p>
 	<p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
 </div>
 <div class="container">
